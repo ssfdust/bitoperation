628a26068271a9788fcde8584b4a8cb5ba2c459a
ssfdust <ssfdust@gmail.com>
2021-07-17 18:07:25 +0800

    :sparkles:升级为16位数的任意打印

--- utils.c
+++ utils.c
@@ -1,10 +1,11 @@
 #include "utils.h"
 #include <stdio.h>
+#include <limits.h>
 
 #define bin2dec_p(...) _bin2dec_p(PP_NARG(__VA_ARGS__), __VA_ARGS__)
 #define dec2bin_p(...) _dec2bin_p(PP_NARG(__VA_ARGS__), __VA_ARGS__)
 
-unsigned dec2bin(unsigned num) {
+unsigned long long dec2bin(unsigned long long num) {
   if (num == 0) {
     return 0;
   } else if (num == 1) {
@@ -13,7 +14,7 @@ unsigned dec2bin(unsigned num) {
   return (num % 2) + 10 * dec2bin(num / 2);
 }
 
-unsigned bin2dec(unsigned num) {
+unsigned long long bin2dec(unsigned long long num) {
   if (num == 0) {
     return 0;
   } else if (num == 1) {
@@ -26,7 +27,7 @@ void _dec2bin_p(size_t argc, ...) {
   va_list ap;
   va_start(ap, argc);
   for (int i = 0; i < argc; i++) {
-    printf("%08u\n", dec2bin(va_arg(ap, unsigned)));
+    printf("%016llu\n", dec2bin(va_arg(ap, unsigned long long)));
   }
   va_end(ap);
 }
@@ -35,7 +36,7 @@ void _bin2dec_p(size_t argc, ...) {
   va_list ap;
   va_start(ap, argc);
   for (int i = 0; i < argc; i++) {
-    printf("%08u\n", bin2dec(va_arg(ap, unsigned)));
+    printf("%016llu\n", bin2dec(va_arg(ap, unsigned long long)));
   }
   va_end(ap);
 }
--- utils.h
+++ utils.h
@@ -25,8 +25,8 @@
       25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, \
       6, 5, 4, 3, 2, 1, 0
 
-unsigned dec2bin(unsigned);
-unsigned bin2dec(unsigned);
+unsigned long long dec2bin(unsigned long long);
+unsigned long long bin2dec(unsigned long long);
 void _bin2dec_p(size_t, ...);
 void _dec2bin_p(size_t, ...);
 void formula_relation(int, int);
