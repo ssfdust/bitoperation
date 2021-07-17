8e424d7bebac1e73bca350b00a2ee4d330205331
ssfdust <ssfdust@gmail.com>
2021-07-17 14:28:45 +0800

    add print function

--- utils.c
+++ utils.c
@@ -1,6 +1,9 @@
 #include "utils.h"
 #include <stdio.h>
 
+#define bin2dec_p(...) _bin2dec_p(PP_NARG(__VA_ARGS__), __VA_ARGS__)
+#define dec2bin_p(...) _dec2bin_p(PP_NARG(__VA_ARGS__), __VA_ARGS__)
+
 unsigned dec2bin(unsigned num) {
   if (num == 0) {
     return 0;
@@ -16,9 +19,23 @@ unsigned bin2dec(unsigned num) {
   } else if (num == 1) {
     return 1;
   }
-  return (num % 10) + 2 * dec2bin(num / 10);
+  return (num % 10) + 2 * bin2dec(num / 10);
 }
 
-void formula_relation(int a, int b) {
-  printf("%u -> %u\n", dec2bin((unsigned)a), dec2bin((unsigned)b));
+void _dec2bin_p(size_t argc, ...) {
+  va_list ap;
+  va_start(ap, argc);
+  for (int i = 0; i < argc; i++) {
+    printf("%08u\n", dec2bin(va_arg(ap, unsigned)));
+  }
+  va_end(ap);
+}
+
+void _bin2dec_p(size_t argc, ...) {
+  va_list ap;
+  va_start(ap, argc);
+  for (int i = 0; i < argc; i++) {
+    printf("%08u\n", bin2dec(va_arg(ap, unsigned)));
+  }
+  va_end(ap);
 }
